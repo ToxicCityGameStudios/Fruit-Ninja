55bc33e5ed573bef1537fe5a4f70ef33a7d56c5a
newhite70 <newhite7070@gmail.com>
2022-01-15 17:18:51 -0500

    Test commit after moving repo to organization level

--- Assets/Scripts/NewBehaviourScript.cs
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -7,7 +7,7 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Hello World
     }
 
     // Update is called once per frame
