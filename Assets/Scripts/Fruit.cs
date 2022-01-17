d1e6fb08ebdbcd5d3c089b1608b6ab0b7c15dd85
newhite70 <newhite7070@gmail.com>
2022-01-16 19:50:36 -0500

    Started creating the fruit

---
+++ Assets/Scripts/Fruit.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fruit : MonoBehaviour
+{
+    public GameObject slicedFruitPrefab;
+
+    public void CreateSlicedFruit()
+    {
+        GameObject inst = Instantiate(slicedFruitPrefab, transform.position, transform.rotation);
+
+        Destroy(gameObject);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CreateSlicedFruit();
+        }
+    }
+}
--- Assets/Scripts/NewBehaviourScript.cs
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    public GameObject slicedFruitPrefab;
+
+    public void CreateSlicedFruit()
+    {
+        GameObject inst = Instantiate(slicedFruitPrefab, transform.position, transform.rotation);
+
+        Destroy(gameObject);
+    }
     // Start is called before the first frame update
     void Start()
     {
-        Hello World
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CreateSlicedFruit();
+        }
     }
 }
