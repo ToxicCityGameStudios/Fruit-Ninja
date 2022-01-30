da437be180cef0e7890835e3f6cbae1ee81b6acd
newhite70 <newhite7070@gmail.com>
2022-01-30 15:23:12 -0500

    OnClick() requires that the GameManager game object to be connect to it.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -25,6 +25,7 @@ public class GameManager : MonoBehaviour
 
     public void RestartGame()
     {
+        Time.timeScale = 1;
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }    
 }
